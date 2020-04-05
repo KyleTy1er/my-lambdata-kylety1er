3a6a754082e0f3db3d2aa83d748cd252216ce475
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-04-05 10:00:13 -0700

    new file

---
+++ my_lambdata/haha.py
@@ -0,0 +1,12 @@
+
+
+# def relol(n):
+#     return n
+
+
+y = (input("Please enter something here: "))
+
+if y == "this message":
+    print('then this message')
+else:
+    print ("not this message")
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -3,8 +3,14 @@
 
 import pandas
 
+from my_lambdata.my_mod import enlarge
+
 print ("SOMETHING HUH")
 
 df = pandas.DataFrame({"x":[1,2,3], "y":[4,5,6]})
 
 print(df.head())
+
+x=5
+
+print (enlarge(x))
