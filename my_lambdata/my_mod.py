46e7eb7ce651fcba44ded01c738d9f55479d0676
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-04-07 14:37:40 -0700

    Assignment completed

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -34,8 +34,7 @@ def feature_keeper(dataframe, target):
     train_features = dataframe.drop(columns=[target])
 
     # Get a list of the numeric features
-    numeric_features = train_features.select_dtypes(include='number') /
-    .columns.tolist()
+    numeric_features = train_features.select_dtypes(include='number').columns.tolist()
 
     # Get a series with the cardinality of the nonnumeric features
     cardinality = train_features.select_dtypes(exclude='number').nunique()
@@ -48,5 +47,46 @@ def feature_keeper(dataframe, target):
 
     return features
 
+# Class that performs some common model preparation tasks
 
-class 
+
+class Model_Prep():
+
+    def __init__(self, dataframe, target=None):
+        self.dataframe = dataframe
+        self.target = target
+
+    # Drop target variable in a dataframe
+    def drop_target(self):
+        train_features = self.dataframe.drop(columns=[self.target])
+        return train_features
+
+    # Get a list of the numeric features
+    def numeric_features(self):
+        numeric_features = self.dataframe.select_dtypes(include='number').columns.tolist()
+        return numeric_features
+
+    # Get a list of all categorical features with cardinality <= 50
+    def cardinality(self):
+        cardinality = self.dataframe.select_dtypes(exclude='number').nunique()
+        categorical_features = cardinality[cardinality <= 50].index.tolist()
+        return categorical_features
+
+    # Combine list of categorical/numeric features
+    def numeric_cat_combine(self):
+        numeric_features = self.dataframe.select_dtypes(include='number').columns.tolist()
+        cardinality = self.dataframe.select_dtypes(exclude='number').nunique()
+        categorical_features = cardinality[cardinality <= 50].index.tolist()
+        features = numeric_features + categorical_features
+        return features
+
+
+if __name__ == "__main__":
+
+    df = pd.read_csv('https://raw.githubusercontent.com/KyleTy1er/Build-Week-Unit-2/master/fighters_df.csv', index_col=0)
+    df_test = Model_Prep(df, 'is_winner')
+
+    print (df_test.drop_target())
+    print (df_test.numeric_features())
+    print (df_test.cardinality())
+    print (df_test.numeric_features())
